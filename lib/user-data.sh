45882cb6044acce5a8a824ba66db2be3e091f88b
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-07-30 23:19:37 +0300

    .

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install python3-pip postgresql libpq-dev
+apt-get update
+apt-get install python3-pip postgresql libpq-dev
 git clone https://github.com/Koji-Sunioj/fastapi-wages.git
 cd fastapi-wages/
 pip install -r requirements.txt

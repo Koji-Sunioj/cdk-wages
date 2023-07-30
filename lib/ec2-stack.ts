2f712cd732606df5eb3f567bcd604c1ea02edc71
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-07-30 20:32:32 +0300

    ec

--- lib/ec2-stack.ts
+++ lib/ec2-stack.ts
@@ -13,5 +13,21 @@ export class Ec2Stack extends Stack {
     const ec2SecurityGroup = new ec2.SecurityGroup(this, "WagesSecurityGroup", {
       vpc: props.vpc,
     });
+
+    ec2SecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
+
+    const instance = new ec2.Instance(this, "WagesVM", {
+      instanceType: new ec2.InstanceType("t3.micro"),
+      machineImage: ec2.MachineImage.genericLinux({
+        "eu-north-1": "ami-0989fb15ce71ba39e",
+      }),
+      vpc: props.vpc,
+      instanceName: "WagesVM",
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      securityGroup: ec2SecurityGroup,
+      keyName: "hey",
+    });
   }
 }

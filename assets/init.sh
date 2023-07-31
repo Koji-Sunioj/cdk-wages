235c84912193e73bb1d17a9c8824b67f391c3bd9
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-08-01 00:40:40 +0300

    sh

--- assets/init.sh
+++ assets/init.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 apt update -y
 apt install python3-pip postgresql libpq-dev nginx -y
 git clone https://github.com/Koji-Sunioj/fastapi-wages.git
--- assets/postinit.sh
+++ assets/postinit.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
 source /etc/environment
-printf 'server {\n\
-        listen 80;\n\
-        server_name %s;\n\
-        location / {\n\
-            proxy_pass http://127.0.0.1:8000;\n\
-      }\n\
+printf \
+'server {
+        listen 80;
+        server_name %s;
+        location / {
+            proxy_pass http://127.0.0.1:8000;
+      }
 }' $PUBLICIP > /etc/nginx/sites-enabled/fastapi_nginx
 service nginx restart
 uvicorn main:app --reload

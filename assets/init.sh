0a62e45e7851d5ec18d63533c4be550755b2d868
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-07-31 22:30:23 +0300

    ip

--- assets/init.sh
+++ assets/init.sh
@@ -4,4 +4,5 @@ apt install python3-pip postgresql libpq-dev nginx -y
 git clone https://github.com/Koji-Sunioj/fastapi-wages.git
 cd fastapi-wages/
 pip install -r requirements.txt
-PUBLICIP=$(echo curl http://checkip.amazonaws.com) >> /etc/environment
+PUBLICIP=$(curl http://checkip.amazonaws.com)
+echo "PUBLICIP=$PUBLICIP" >> /etc/environment

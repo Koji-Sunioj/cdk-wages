e39728c733f8a50f90b99ffa3a405e76a1b9f6a0
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-07-31 22:56:18 +0300

    testing public http

--- lib/ec2-stack.ts
+++ lib/ec2-stack.ts
@@ -63,17 +63,17 @@ export class Ec2Stack extends Stack {
       `echo "FE_SECRET=${props.frontEndSecret}" >> /etc/environment`
     );
     instance.addUserData("source /etc/environment");
-    /* instance.addUserData(
-      `echo 'server {\
-      listen 80;\
-      server_name ${instance.instancePublicIp};\
-      location / {\
-          proxy_pass http://127.0.0.1:8000;\
+    instance.addUserData(
+      "printf 'server {\
+        listen 80;\
+        server_name %s\
+        location / {\
+            proxy_pass http://127.0.0.1:8000;\
       }\
-      }' >> /etc/nginx/sites-enabled/fastapi_nginx`
+    }' $PUBLICIP > /etc/nginx/sites-enabled/fastapi_nginx"
     );
     instance.addUserData(
       "service nginx restart && cd fastapi-wages/ && uvicorn main:app --reload"
-    ); */
+    );
   }
 }
